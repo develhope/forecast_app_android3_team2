9f0fabbb380e9da9b3e48f2ad0d14180152fef8b
pepprond <rondelli95@gmail.com>
2023-01-25 19:03:41 +0100

    ho cancellato la recycler view del search fragment,

--- app/src/main/java/co/develhope/meteoapp/SearchPage/Model/SearchItem.kt
+++
@@ -1,6 +0,0 @@
-package co.develhope.meteoapp.SearchPage.Model
-
-sealed class SearchItem{
-    data class RicercheRecenti(val string: String):SearchItem()
-    data class SearchCity(val cityName: String):SearchItem()
-}
