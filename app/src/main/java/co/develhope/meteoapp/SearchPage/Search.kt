f333b9d863dd37d7e2169c5fe59c7891e13f3add
pepprond <rondelli95@gmail.com>
2023-01-25 19:04:30 +0100

    ho cancellato la recycler view del search fragment,

--- app/src/main/java/co/develhope/meteoapp/SearchPage/Search.kt
+++ app/src/main/java/co/develhope/meteoapp/SearchPage/Search.kt
@@ -5,13 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.LinearLayoutManager
-import co.develhope.meteoapp.R
-import co.develhope.meteoapp.SearchPage.Adapter.SearchAdapter
-import co.develhope.meteoapp.SearchPage.Adapter.SearchAdapter.Companion.TYPE_RICERCHE_RECENTI
-import co.develhope.meteoapp.SearchPage.Adapter.SearchAdapter.Companion.TYPE_SEARCH_CITY
-import co.develhope.meteoapp.SearchPage.Data.SearchDataItem.loadSearchData
-import co.develhope.meteoapp.databinding.FragmentHomepageBinding
 import co.develhope.meteoapp.databinding.FragmentSearchBinding
 
 class Search : Fragment() {
@@ -29,14 +22,4 @@ class Search : Fragment() {
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-       val adapterCitySearch = SearchAdapter(TYPE_SEARCH_CITY,loadSearchData())
-        binding.RVSearch.adapter =adapterCitySearch
-        binding.RVSearch.layoutManager =LinearLayoutManager(view.context)
-
-        val adapterRicercheRecenti= SearchAdapter(TYPE_RICERCHE_RECENTI, loadSearchData())
-        binding.RVSearch.adapter =adapterRicercheRecenti
-        binding.RVSearch.layoutManager =LinearLayoutManager(view.context)
-
-    }
 }
