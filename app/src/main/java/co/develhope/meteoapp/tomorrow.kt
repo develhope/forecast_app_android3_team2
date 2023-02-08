0f50cf144698f5a5143af4ed5ec8619174e9bc75
pepprond <rondelli95@gmail.com>
2023-02-08 12:16:50 +0100

    ho sistemato i file per il merge

--- app/src/main/java/co/develhope/meteoapp/tomorrow.kt
+++ app/src/main/java/co/develhope/meteoapp/tomorrow.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [SpecificDay.newInstance] factory method to
+ * Use the [tomorrow.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SpecificDay : Fragment() {
+class tomorrow : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -32,10 +32,8 @@ class SpecificDay : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_specific_day, container, false)
+        return inflater.inflate(R.layout.fragment_tomorrow, container, false)
     }
-
     companion object {
         /**
          * Use this factory method to create a new instance of
