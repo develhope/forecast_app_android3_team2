8039390823a284784316b005ca25e31d81d399f1
pepprond <rondelli95@gmail.com>
2023-02-20 14:27:49 +0100

    refactor dei dto

--- app/src/main/java/co/develhope/meteoapp/network/weather/dto/CurrentWeatherX.kt
+++
@@ -1,9 +0,0 @@
-package co.develhope.meteoapp.network.weather.dto
-
-data class CurrentWeatherX(
-    val temperature: Double,
-    val time: String,
-    val weathercode: Int,
-    val winddirection: Double,
-    val windspeed: Double
-)
--- app/src/main/java/co/develhope/meteoapp/network/weather/dto/WeeklySummary.kt
+++ app/src/main/java/co/develhope/meteoapp/network/weather/dto/WeeklySummary.kt
@@ -1,14 +1,73 @@
 package co.develhope.meteoapp.network.weather.dto
+import com.google.gson.annotations.SerializedName
+
 
 data class WeeklySummary(
-    val current_weather: CurrentWeatherX,
-    val daily: Daily,
-    val daily_units: DailyUnits,
+    @SerializedName("current_weather")
+    val currentWeather: CurrentWeather,
+    @SerializedName("elevation")
     val elevation: Double,
-    val generationtime_ms: Double,
+    @SerializedName("generationtime_ms")
+    val generationtimeMs: Double,
+    @SerializedName("hourly")
+    val hourly: Hourly,
+    @SerializedName("hourly_units")
+    val hourlyUnits: HourlyUnits,
+    @SerializedName("latitude")
     val latitude: Double,
+    @SerializedName("longitude")
     val longitude: Double,
+    @SerializedName("timezone")
     val timezone: String,
-    val timezone_abbreviation: String,
-    val utc_offset_seconds: Int
+    @SerializedName("timezone_abbreviation")
+    val timezoneAbbreviation: String,
+    @SerializedName("utc_offset_seconds")
+    val utcOffsetSeconds: Int
+)
+
+data class CurrentWeather(
+    @SerializedName("temperature")
+    val temperature: Double,
+    @SerializedName("time")
+    val time: String,
+    @SerializedName("weathercode")
+    val weathercode: Int,
+    @SerializedName("winddirection")
+    val winddirection: Double,
+    @SerializedName("windspeed")
+    val windspeed: Double
+)
+
+data class Hourly(
+    @SerializedName("rain")
+    val rain: List<Double>,
+    @SerializedName("showers")
+    val showers: List<Double>,
+    @SerializedName("snowfall")
+    val snowfall: List<Double>,
+    @SerializedName("temperature_2m")
+    val temperature2m: List<Double>,
+    @SerializedName("time")
+    val time: List<String>,
+    @SerializedName("weathercode")
+    val weathercode: List<Int>,
+    @SerializedName("windspeed_10m")
+    val windspeed10m: List<Double>
+)
+
+data class HourlyUnits(
+    @SerializedName("rain")
+    val rain: String,
+    @SerializedName("showers")
+    val showers: String,
+    @SerializedName("snowfall")
+    val snowfall: String,
+    @SerializedName("temperature_2m")
+    val temperature2m: String,
+    @SerializedName("time")
+    val time: String,
+    @SerializedName("weathercode")
+    val weathercode: String,
+    @SerializedName("windspeed_10m")
+    val windspeed10m: String
 )
