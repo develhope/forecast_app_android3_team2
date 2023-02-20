3311efc53e7210a24b1ab2b4abb4ac7eb5da6f67
pantilimonbianca <pantilimonbianca00@gmail.com>
2023-02-20 15:31:05 +0100

    prova

--- app/src/main/java/co/develhope/meteoapp/network/weather/WeatherService.kt
+++ app/src/main/java/co/develhope/meteoapp/network/weather/WeatherService.kt
@@ -9,5 +9,6 @@ interface WeatherService {
     @GET("forecast?hourly=temperature_2m,rain,showers,snowfall,weathercode,windspeed_10m&current_weather=true&timezone=Europe%2FBerlin&start_date=2022-10-03&end_date=2022-10-03")
     suspend fun GetDaySummary (@Query("latitude") latitude : Double = 41.8955, @Query("longitude") longitude : Double = 12.4823):DaySummary
 
-
+    @GET("forecast?daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum,rain_sum&current_weather=true&timezone=Europe%2FBerlin")
+    suspend fun GetWeeklySummary(@Query("latitude") latitude : Double = 41.8955, @Query("longitude") longitude : Double = 12.4823):WeeklySummary
 }
