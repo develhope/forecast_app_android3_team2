b325d77996415cfe5c474055a41e939d6e7bbdeb
pepprond <rondelli95@gmail.com>
2023-03-03 15:59:36 +0100

    ho aggiunto i parametri mancanti per il weathercode

--- app/src/main/java/co/develhope/meteoapp/data/domainmodel/WeatherType.kt
+++ app/src/main/java/co/develhope/meteoapp/data/domainmodel/WeatherType.kt
@@ -3,22 +3,29 @@ package co.develhope.meteoapp.data.domainmodel
 import co.develhope.meteoapp.R
 
 enum class WeatherType {
-    SUNNY, CLOUDY, RAINY;
+    SUNNY, CLOUDY, RAINY, FOGGY, TEMPEST, HEAVYRAIN;
 
     fun setIconWeatherType(): Int {
         return when (this) {
             SUNNY -> R.drawable.sun
             RAINY -> R.drawable.rain
             CLOUDY -> R.drawable.sun_cloud
+            TEMPEST -> R.drawable.rain
+            FOGGY -> R.drawable.sun_cloud
+            HEAVYRAIN ->  R.drawable.rain
+            else -> R.drawable.moon
         }
     }
 
 }
 fun Int?.getWeatherType(): WeatherType {
     return when (this) {
-        0, 1 -> WeatherType.SUNNY
-        2, 3 -> WeatherType.RAINY
-        4, 5 -> WeatherType.CLOUDY
+        0  -> WeatherType.SUNNY
+        1, 2, 3 -> WeatherType.CLOUDY
+        in 45..48 -> WeatherType.FOGGY
+        in 51..77 -> WeatherType.RAINY
+        80, 81, 82 -> WeatherType.HEAVYRAIN
+        in 95..99 -> WeatherType.HEAVYRAIN
         else -> WeatherType.CLOUDY
 
     }
