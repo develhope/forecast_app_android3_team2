74d01c1c39a99b74bcc4f54fe677e558a34f6bf5
pantilimonbianca <pantilimonbianca00@gmail.com>
2023-03-16 17:52:58 +0100

    specific day viewModel

--- app/src/main/java/co/develhope/meteoapp/ui/SpecificDayFragment.kt
+++ app/src/main/java/co/develhope/meteoapp/ui/SpecificDayFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import co.develhope.meteoapp.data.DataSource
@@ -15,6 +16,7 @@ import co.develhope.meteoapp.databinding.FragmentSpecificDayBinding
 import co.develhope.meteoapp.network.NetworkProvider
 import co.develhope.meteoapp.ui.adapter.SpecificDayAdapter
 import co.develhope.meteoapp.ui.adapter.SpecificDayModel
+import co.develhope.meteoapp.ui.model.SpecificDayViewModel
 import kotlinx.coroutines.launch
 import org.threeten.bp.OffsetDateTime
 
@@ -22,7 +24,7 @@ import org.threeten.bp.OffsetDateTime
 class SpecificDayFragment : Fragment() {
 
     private var _binding: FragmentSpecificDayBinding? = null
-
+private lateinit var viewModel: SpecificDayViewModel
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -30,6 +32,8 @@ class SpecificDayFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentSpecificDayBinding.inflate(inflater, container, false)
+        viewModel= ViewModelProvider(this).get(SpecificDayViewModel::class.java)
+
         return binding.root
 
     }
@@ -41,35 +45,15 @@ class SpecificDayFragment : Fragment() {
 
     override fun onStart() {
         super.onStart()
-        getHourlyForecast()
-    }
+        viewModel.getHourlyForecast()
 
-    private fun getHourlyForecast() {
-        lifecycleScope.launch {
-            try {
-                val hourlyForecast = NetworkProvider().getDailySummary(
-                    DataSource.getSelectedPlace()?.lat ?: 38.116667,
-                    DataSource.getSelectedPlace()?.log ?: 13.366667,
-                    getDate(),
-                    getDate()
-                )
+        viewModel.specificDayResult.observe(viewLifecycleOwner){
+            val specificDayItems: List<SpecificDayModel> = createListToShow(it)
 
-                Log.d("prova card specific day", "${hourlyForecast}")
-                val specificDayItems: List<SpecificDayModel> = createListToShow(hourlyForecast)
-
-                val adapter = SpecificDayAdapter(specificDayItems)
-                binding.itemSpecificday.adapter = adapter
-
-            } catch (e: Exception) {
-                e.printStackTrace()
-                Log.d("provadaily", "errore")
-            }
+            val adapter = SpecificDayAdapter(specificDayItems)
+            binding.itemSpecificday.adapter = adapter
         }
     }
-
-
-
-    private fun getDate(): OffsetDateTime = OffsetDateTime.now()
 
 
 
@@ -77,10 +61,10 @@ class SpecificDayFragment : Fragment() {
     private fun createListToShow(list: List<HourlyForecast>): List<SpecificDayModel> {
         val listToReturn = mutableListOf<SpecificDayModel>()
         val filteredList =
-            list.filter { hourlyForecast -> hourlyForecast.hourlySpecificDay.time.isAfter(getDate()) }
+            list.filter { hourlyForecast -> hourlyForecast.hourlySpecificDay.time.isAfter(DataSource.getDate()) }
 
 
-        listToReturn.add(SpecificDayModel.SpecificDayTitle(DataSource.getSelectedPlace()!!, date = getDate()))
+        listToReturn.add(SpecificDayModel.SpecificDayTitle(DataSource.getSelectedPlace()!!, date = DataSource.getDate()))
         listToReturn.add(SpecificDayModel.SpecificDayHourly(filteredList.first()))
         listToReturn.add(SpecificDayModel.SpecificDayCard(filteredList.first().cardSpecificDay))
 
---
+++ app/src/main/java/co/develhope/meteoapp/ui/model/SpecificDayViewModel.kt
@@ -0,0 +1,34 @@
+package co.develhope.meteoapp.ui.model
+
+import android.util.Log
+import androidx.lifecycle.*
+import co.develhope.meteoapp.data.DataSource
+import co.develhope.meteoapp.data.DataSource.getDate
+import co.develhope.meteoapp.data.domainmodel.HourlyForecast
+import co.develhope.meteoapp.network.NetworkProvider
+import co.develhope.meteoapp.ui.adapter.SpecificDayAdapter
+import co.develhope.meteoapp.ui.adapter.SpecificDayModel
+import kotlinx.coroutines.launch
+
+class SpecificDayViewModel: ViewModel()  {
+    private var _specificDayResult = MutableLiveData<List<HourlyForecast>>()
+    val specificDayResult : LiveData<List<HourlyForecast>> get() = _specificDayResult
+
+    fun getHourlyForecast() {
+        viewModelScope.launch {
+            try {
+                val hourlyForecast = NetworkProvider().getDailySummary(
+                    DataSource.getSelectedPlace()?.lat ?: 38.116667,
+                    DataSource.getSelectedPlace()?.log ?: 13.366667,
+                    getDate(),
+                    getDate()
+                )
+                _specificDayResult.value = hourlyForecast
+
+            } catch (e: Exception) {
+                e.printStackTrace()
+                Log.d("provadaily", "errore")
+            }
+        }
+    }
+}
